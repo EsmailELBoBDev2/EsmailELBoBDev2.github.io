6f7968473910d6a77d4113896794fcf809ddab61
Esmail EL BoB <esmailelbobforgames@gmail.com>
2018-07-22 17:39:11 +0200

    Add files via upload

---
+++ my personal website/indexar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang = "ar-EG">
+ <head>
+ <title>Esmail EL BoB [Personal-Beta Website] (V.2)</title>
+ <meta charset = "UTF-8">
+ <link rel="stylesheet" type="text/css" href="stylear.css">
+ </head>
+
+ <header>
+<ul>
+  <li><a href="index.php">English Versopn</a></li>
+</ul>
+</header>
+
+<body background="img/background.png">
+  <div class="container-fluid">
+  <div class="row">
+  <div id="logo"><img src="img/logo.png"></div>
+  <div id="hello">أهلا</div>
+  <div id="my-name">انا إسماعيل محمد</div>
+  <div id="noob">افشل مبرمج هتشوفه فى حياتك هههه</div>
+  </div>
+  </div>
+
+    <form id="button1place">
+        <input id="button" type="button" value="إيميلاتى" onclick="window.location.href='https://pastebin.com/raw/eA3Vv6sw'" />
+    </form> 
+
+    <form id="button2place">
+        <input id="button2" type="button" value="" onclick="window.location.href='easteregg.php'" />
+    </form> 
+
+</body>
+
+</html>
