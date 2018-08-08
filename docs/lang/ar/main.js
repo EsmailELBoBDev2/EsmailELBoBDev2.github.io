9e1c938eb8f0e3a85f9e7d97f3c965d46afb4445
Esmail EL BoB <esmailelbobforgames@gmail.com>
2018-08-08 14:16:54 +0200

    Add files via upload

---
+++ docs/lang/ar/main.js
@@ -0,0 +1,20 @@
+let ed = new Audio("songs/ed.mp3");
+let drake = new Audio("songs/drake.mp3");
+let randomwords = [
+  "انا مبرمج محترف",
+  "أفضل مبمرج هتشوفة فى حياتك هههه",
+  "علشان كدا متقوليش يا بيبى <br> -إيد شيران",
+  "كيكى انت بتحبينى ؟ <br> -دريك"
+];
+
+let randomItem = Math.floor(Math.random() * randomwords.length);
+document.getElementById("randomwords").innerHTML = randomwords[randomItem];
+function play() {
+  if (randomItem ==2) {
+    ed.play();
+  } else if (randomItem ==3) {
+  drake.play();
+  }
+}
+
+play(); 
