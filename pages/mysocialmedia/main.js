2aa58bf28c4b002ae712e44a6adf59693cb0f69c
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-01-16 22:29:57 +0200

    Update main.js

--- pages/mysocialmedia/main.js
+++ pages/mysocialmedia/main.js
@@ -21,5 +21,5 @@ function lightmode() {
 }
 
 function jabberPopup() {
-  alert("My XMPP/Jabber Username: esmailelbob@jab.im '\n' My OTR Fingerprint: 600A2A25 894982CF 0AC47D47 B87A3627 62FAEB15");
+  alert("My XMPP/Jabber Username: esmailelbob@jab.im \n My OTR Fingerprint: 600A2A25 894982CF 0AC47D47 B87A3627 62FAEB15");
 }
