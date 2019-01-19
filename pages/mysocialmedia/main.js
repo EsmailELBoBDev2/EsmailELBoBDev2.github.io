1365d8d6a9f0f05b60db1a59135213c319276b03
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-01-19 16:01:23 +0200

    ­

---
+++ pages/mysocialmedia/main.js
@@ -0,0 +1,25 @@
+function darkmode() {
+  document.body.style.backgroundColor = "#282c34";
+  document.getElementById("site-name").style.color = "#939caa";
+  document.getElementById("•").style.color = "#939caa";
+  document.getElementById("dark-mode").style.visibility = "hidden";
+  document.getElementById("light-mode").style.cssText = "color: #939caa; visibility: visible;";
+  document.getElementById("jabber").style.color = "white";
+  document.getElementById("back").style.cssText = "color: #939caa; background-color: #282c34; border-color: white; ";
+
+}
+darkmode();
+
+function lightmode() {
+  document.body.style.backgroundColor = "white";
+  document.getElementById("site-name").style.color = "black";
+  document.getElementById("•").style.color = "black";
+  document.getElementById("light-mode").style.visibility = "hidden";
+  document.getElementById("dark-mode").style.cssText = "color: black; visibility: visible;";
+  document.getElementById("jabber").style.color = "black";
+  document.getElementById("back").style.cssText = "color: black; background-color: white; border-color: blue;";
+}
+
+function jabberPopup() {
+  alert("My XMPP/Jabber Username: esmailelbob@chatserver.space\n\nMy OMEMO Fingerprint: BBC6408D C12BEC96 C4874E04 0D6B6426 3CFC9D8D 858806CC 24349FB3 40BB7068");
+}
