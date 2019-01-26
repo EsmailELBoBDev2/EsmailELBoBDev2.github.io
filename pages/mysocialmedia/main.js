5bbc4d12c275b31bec76a73edbfea11d9dccb670
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-01-26 23:11:54 +0200

    Update main.js

--- pages/mysocialmedia/main.js
+++ pages/mysocialmedia/main.js
@@ -5,6 +5,7 @@ function darkmode() {
   document.getElementById("dark-mode").style.visibility = "hidden";
   document.getElementById("light-mode").style.cssText = "color: #939caa; visibility: visible;";
   document.getElementById("jabber").style.color = "white";
+  document.getElementById("wire").style.color = "white";
   document.getElementById("back").style.cssText = "color: #939caa; background-color: #282c34; border-color: white; ";
 
 }
@@ -17,6 +18,7 @@ function lightmode() {
   document.getElementById("light-mode").style.visibility = "hidden";
   document.getElementById("dark-mode").style.cssText = "color: black; visibility: visible;";
   document.getElementById("jabber").style.color = "black";
+  document.getElementById("wire").style.color = "black";
   document.getElementById("back").style.cssText = "color: black; background-color: white; border-color: blue;";
 }
 
