b610971a354d75ff84950d6d3b675f636b3d7310
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-03-04 16:35:54 +0000

    Update main.js

--- pages/accounts/main.js
+++ pages/accounts/main.js
@@ -25,7 +25,7 @@ function lightmode() {
 }
 
 function jabberPopup() {
-  alert("My Jabber Username: esmailelbob@chatserver.space\n\nMy OMEMO Fingerprint: 78d41f668f8fca27607595c38bb4527f0203a286b953b4d1a3de1d695d1deb70");
+  alert("My Jabber Username: esmailelbob@404.city);
 }
 
 function wirePopup() {
