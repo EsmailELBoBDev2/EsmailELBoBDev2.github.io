979d5ffdc9edb3b69be00fcd1bac9566e2c4a004
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-05-17 03:49:31 +0200

    Update main.js

--- pages/accounts/main.js
+++ pages/accounts/main.js
@@ -28,10 +28,6 @@ function wirePopup() {
   alert("My Wire Username: @esmailelbob");
 }
 
-function signalPopup() {
-  alert("My Signal Phone Number: +19152431377");
-}
-
 function myEmail() {
   if (confirm("Want Also See My PGP Public Key ?\n\n(Don't Forget To Allow Pop-ups)")) {
 
